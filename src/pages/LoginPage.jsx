31a25585bde6925b33fd6179f40452812470b2c4
bodoqueen <160500360+bodoqueen@users.noreply.github.com>
2024-02-29 12:09:54 -0500

    💼 - Login y Register Alarcon

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,51 @@
+import { Link } from "react-router-dom";
+
 function Login() {
   return (
     <>
-      <section className="h-screen flex justify-center items-center">
-        <h1 className="font-bold">Aqui va el login 👻</h1>
-      </section> 
+      <section className="h-screen bg-gray-900 flex justify-center items-center">
+        <article className="w-96 bg-white rounded-lg shadow p-6">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900">
+            Iniciar Sesion
+          </h1>
+          
+          <form className="space-y-4 mt-4" action="#">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
+                Tu Correo
+              </label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5" placeholder="name@rustica.com" required />
+            </div>
+
+            <div>
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
+                Contraseña
+              </label>
+              <input type="password" name="password" id="password" placeholder="*********" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5" required />
+            </div>
+
+            <div className="flex items-center justify-end">
+              <a href="#" className="text-sm font-medium text-primary-600 hover:underline">
+                Olvidaste tu Contraseña?
+              </a>
+            </div>
+
+            <button type="submit" className="w-full text-dark bg-lime-200  hover:bg-lime-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+              Entrar
+            </button>
+
+            <p className="text-sm font-light text-gray-500">
+              No Tienes cuenta?
+              <Link to={"/register"} className="font-medium text-primary-600 hover:underline">
+                Registrate
+              </Link>
+            </p>
+          </form>
+
+        </article>
+      </section>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,43 @@
+import { Link } from "react-router-dom";
+
 function Register() {
   return (
     <>
-      <section className="h-screen flex justify-center items-center">
-        <h1 className="font-bold">Aqui va el register 🐍</h1>
-      </section>
+      <section className="h-screen bg-cyan-800 flex justify-center items-center">
+        <article className="w-96 bg-white rounded-lg shadow p-6">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900">
+            Crear cuenta
+          </h1>
+
+          <form className="space-y-4 mt-4" action="#">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
+                Tu Correo 
+              </label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5" placeholder="name@rustica.com" required />
+            </div>
+
+            <div>
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
+                Tu Contraseña
+              </label>
+              <input type="password" name="password" id="password" placeholder="*********" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5" required />
+            </div>
+
+            <button type="submit" className="w-full text-dark bg-rose-100 hover:bg-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+              Crear Cuenta 
+            </button>
+             
+            <p className="text-sm font-light text-gray-500">
+              Ya tienes cuenta?
+              <Link to={"/"} className="font-medium text-primary-600 hover:underline">
+                regresar login
+              </Link>
+            </p>
+
+          </form> 
+          </article>  
+      </section>    
     </>
   )
 }

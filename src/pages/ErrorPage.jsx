b90877667aa31977d482d40d1f4f314e94f2c572
sahebath <lematrye@gmail.com>
2024-02-29 21:21:51 -0500

    🚩- Changes Error Page / Dylan

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,25 @@
 import { useRouteError } from "react-router-dom";
-import './ErrorPage.css'
+import "./ErrorPage.css";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  
+
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Lo sentimos, ha ocurrido un error inesperado.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
+      <p className="error-title">
+        <h1>{error.statusText || error.message}</h1>
       </p>
+      <div className="error-text-container">
+        <p className="error-text">
+          Lo sentimos, ha ocurrido un error inesperado. Por favor, comunícate
+          con nuestro soporte técnico
+        </p>
+        <Link className="btn" to={"/"}>
+          {" "}
+          Volver{" "}
+        </Link>
+      </div>
     </div>
   );
 }
